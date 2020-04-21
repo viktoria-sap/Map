14634bdad028cbf88b396e78b3fac93b5a9ff24c
vika <v.sapunkova@neurobotics.ru>
2020-04-21 16:51:40 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Map.java
@@ -0,0 +1,10 @@
+
+package ru.netology.domain;
+
+import java.util.Collection;
+
+public interface Map<Apps> extends Collection<Apps> {
+    String nameApp = null;
+    String expansion = null;
+
+}
---
+++ src/main/java/ru/netology/manager/FileOpenManager.java
@@ -0,0 +1,9 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Map;
+
+public class FileOpenManager {
+    private Map[] apps = new Map[0];
+
+
+}
